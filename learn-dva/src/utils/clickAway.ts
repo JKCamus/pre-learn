ba179eecd96530e4dc2fc1df772c6a897bd7a6f8
camus <jkcamus@163.com>
2021-07-02 19:13:28 +0800

    feat: useClickAway

---
+++ learn-dva/src/utils/clickAway.ts
@@ -0,0 +1,67 @@
+/*
+ * @Description:
+ * @version:
+ * @Author: camus
+ * @Date: 2021-07-02 18:11:08
+ * @LastEditors: camus
+ * @LastEditTime: 2021-07-02 18:12:29
+ */
+import { MutableRefObject, useRef, useEffect } from 'react';
+export type BasicTarget<T = HTMLElement> = (() => T | null) | T | null | MutableRefObject<T | null | undefined>;
+
+type TargetElement = HTMLElement | Element | Document | Window;
+
+export function getTargetElement(
+  target?: BasicTarget<TargetElement>,
+  defaultElement?: TargetElement,
+): TargetElement | undefined | null {
+  if (!target) {
+    return defaultElement;
+  }
+
+  let targetElement: TargetElement | undefined | null;
+
+  if (typeof target === 'function') {
+    targetElement = target();
+  } else if ('current' in target) {
+    targetElement = target.current;
+  } else {
+    targetElement = target;
+  }
+
+  return targetElement;
+}
+
+const defaultEvent = 'click';
+
+type EventType = MouseEvent | TouchEvent;
+
+export const useClickAway = (
+  onClickAway: (event: EventType) => void,
+  target: BasicTarget | BasicTarget[],
+  eventName: string = defaultEvent,
+) => {
+  const onClickAwayRef = useRef(onClickAway);
+  onClickAwayRef.current = onClickAway;
+
+  useEffect(() => {
+    const handler = (event: any) => {
+      const targets = Array.isArray(target) ? target : [target];
+      if (
+        targets.some((targetItem) => {
+          const targetElement = getTargetElement(targetItem) as HTMLElement;
+          return !targetElement || targetElement?.contains(event.target);
+        })
+      ) {
+        return;
+      }
+      onClickAwayRef.current(event);
+    };
+
+    document.addEventListener(eventName, handler);
+
+    return () => {
+      document.removeEventListener(eventName, handler);
+    };
+  }, [target, eventName]);
+};
